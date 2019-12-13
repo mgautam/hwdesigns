ecf39de2db90f112d2f715c55831b82016061d93
Gautam <gautam.raw@gmail.com>
2019-12-14 08:26:26 +1100

    custom axi stream generator

---
+++ axistreamer/axistreamer.srcs/sources_1/bd/design_1/ip/design_1_axis_data_fifo_0_0/design_1_axis_data_fifo_0_0_stub.v
@@ -0,0 +1,37 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.3 (win64) Build 2018833 Wed Oct  4 19:58:22 MDT 2017
+// Date        : Sat Dec 14 07:49:22 2019
+// Host        : Gautam-PC running 64-bit Service Pack 1  (build 7601)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/Gautam/Vivado/axistreamer/axistreamer.srcs/sources_1/bd/design_1/ip/design_1_axis_data_fifo_0_0/design_1_axis_data_fifo_0_0_stub.v
+// Design      : design_1_axis_data_fifo_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "axis_data_fifo_v1_1_15_axis_data_fifo,Vivado 2017.3" *)
+module design_1_axis_data_fifo_0_0(s_axis_aresetn, s_axis_aclk, s_axis_tvalid, 
+  s_axis_tready, s_axis_tdata, s_axis_tstrb, s_axis_tlast, m_axis_tvalid, m_axis_tready, 
+  m_axis_tdata, m_axis_tstrb, m_axis_tlast, axis_data_count, axis_wr_data_count, 
+  axis_rd_data_count)
+/* synthesis syn_black_box black_box_pad_pin="s_axis_aresetn,s_axis_aclk,s_axis_tvalid,s_axis_tready,s_axis_tdata[31:0],s_axis_tstrb[3:0],s_axis_tlast,m_axis_tvalid,m_axis_tready,m_axis_tdata[31:0],m_axis_tstrb[3:0],m_axis_tlast,axis_data_count[31:0],axis_wr_data_count[31:0],axis_rd_data_count[31:0]" */;
+  input s_axis_aresetn;
+  input s_axis_aclk;
+  input s_axis_tvalid;
+  output s_axis_tready;
+  input [31:0]s_axis_tdata;
+  input [3:0]s_axis_tstrb;
+  input s_axis_tlast;
+  output m_axis_tvalid;
+  input m_axis_tready;
+  output [31:0]m_axis_tdata;
+  output [3:0]m_axis_tstrb;
+  output m_axis_tlast;
+  output [31:0]axis_data_count;
+  output [31:0]axis_wr_data_count;
+  output [31:0]axis_rd_data_count;
+endmodule
